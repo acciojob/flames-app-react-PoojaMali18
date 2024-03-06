7c64f8b26737539ccc6fe407b18ad92f81059ef2
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2024-03-06 07:43:21 +0000

    s

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,71 @@
-import React, { Component, useState } from "react";
-
+import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-
-    let [name1, setName1] = useState('');
-    let [name2, setName2] = useState('');
-    let [relationshipstatus, setRelationshipStatus] = useState('');
-
-    const removeCommonLetters = (str1, str2) => {
-      const result1 = [...str1].filter(char => !str2.includes(char)).join('');
-      const result2 = [...str2].filter(char => !str1.includes(char)).join('');
-
-      return [result1, result2];
+  constructor(props) {
+    super(props);
+    this.state = {
+      name1: '',
+      name2: '',
+      relationshipStatus: ''
     };
+  }
 
-    const clearInputs = () => {
-        setName1('');
-        setName2('');
-        setRelationshipStatus('');
-      };
+  removeCommonLetters = (str1, str2) => {
+    const result1 = [...str1].filter(char => !str2.includes(char)).join('');
+    const result2 = [...str2].filter(char => !str1.includes(char)).join('');
 
-    function calculateRelationship() {
-      let [res1, res2] = removeCommonLetters(name1, name2);
-      let sum = (res1 + res2) % 6;
-      switch (sum) {
-        case 1:
-          setRelationshipStatus('Friends');
-          break;
-        case 2:
-          setRelationshipStatus('Love');
-          break;
-        case 3:
-          setRelationshipStatus('Affection');
-          break;
-        case 4:
-          setRelationshipStatus('Marriage');
-          break;
-        case 5:
-          setRelationshipStatus('Enemy');
-          break;
-        case 0:
-          setRelationshipStatus('Siblings');
-          break;
-        default:
-          setRelationshipStatus('Please Enter valid input');
-      }
+    return [result1, result2];
+  };
+
+  clearInputs = () => {
+    this.setState({
+      name1: '',
+      name2: '',
+      relationshipStatus: ''
+    });
+  };
+
+  calculateRelationship = () => {
+    const [res1, res2] = this.removeCommonLetters(this.state.name1, this.state.name2);
+    const sum = (res1.length + res2.length) % 6;
+
+    switch (sum) {
+      case 1:
+        this.setState({ relationshipStatus: 'Friends' });
+        break;
+      case 2:
+        this.setState({ relationshipStatus: 'Love' });
+        break;
+      case 3:
+        this.setState({ relationshipStatus: 'Affection' });
+        break;
+      case 4:
+        this.setState({ relationshipStatus: 'Marriage' });
+        break;
+      case 5:
+        this.setState({ relationshipStatus: 'Enemy' });
+        break;
+      case 0:
+        this.setState({ relationshipStatus: 'Siblings' });
+        break;
+      default:
+        this.setState({ relationshipStatus: 'Please Enter valid input' });
     }
+  };
 
-
-
-
-
+  render() {
     return (
       <div id="main">
         <form>
-          <input type="text" id="input1" data-testid="input1" onChange={(e) => setName1(e.target.value)} value={name1} placeholder="Enter Name 1" />
-          <input type="text" id="input2" data-testid="input2" onChange={(e) => setName2(e.target.value)} value={name2} placeholder="Enter Name 2" />
-          <button onClick="calculateRelationship()" data-testid="calculate_relationship">Calculate Relationship Future</button>
-          <button onClick="clearInputs()" data-testid="clear">Clear</button>
-          <div id="relationshipResult"></div>
+          <input type="text" id="input1" data-testid="input1" onChange={(e) => this.setState({ name1: e.target.value })} value={this.state.name1} placeholder="Enter Name 1" />
+          <input type="text" id="input2" data-testid="input2" onChange={(e) => this.setState({ name2: e.target.value })} value={this.state.name2} placeholder="Enter Name 2" />
+          <button onClick={this.calculateRelationship} data-testid="calculate_relationship">Calculate Relationship Future</button>
+          <button onClick={this.clearInputs} data-testid="clear">Clear</button>
+          <div id="relationshipResult">{this.state.relationshipStatus}</div>
         </form>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
